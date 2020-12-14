7f85e0a78e34fd5d02e52cf1fc6d867bc7d5549f
andresgr20 <andresgr2094@hotmail.com>
2020-12-14 10:38:29 -0500

    Added template for store

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+        <aside>
+          Shopping Categories
+        </aside>
+        <main>
+          <ul>
+            <li> Product 1</li>
+            <li> Product 2</li>
+            <li> Product 3</li>
+            <li> Product 4</li>
+          </ul>
+        </main>
+        <footer>
+          All rigths reserved.
+        </footer>
+    </div>
+  );
+}
+
+export default App;
